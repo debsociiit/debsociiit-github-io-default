3e559e74edb22dc4d5db1135c9c0c27328d4638c
Mugundan Kottur Suresh <mugundan.kottur@students.iiit.ac.in>
2021-08-09 01:11:25 +0530

    Midway to combining existing html with navbar

--- js/navbar.js
+++ js/navbar.js
@@ -2,9 +2,9 @@ function toggle() {
     var x = document.getElementById("navbar");
     if (x.className === "topnav") {
       x.className += " open";
-      document.getElementById("iconPic").src = "./up.svg";
+      document.getElementById("iconPic").src = "../resources/up.svg";
     } else {
       x.className = "topnav";
-      document.getElementById("iconPic").src = "./down.svg";
+      document.getElementById("iconPic").src = "../resources/down.svg";
     }
   }
