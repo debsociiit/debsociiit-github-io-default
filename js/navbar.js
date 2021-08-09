9ab25a1efeadbfe79b733c9cbf3b56d522b985bd
Mugundan Kottursuresh <mugundan.kottur@gmail.com>
2021-08-10 00:37:08 +0530

    Organized Icons

--- js/navbar.js
+++ js/navbar.js
@@ -13,21 +13,21 @@ class Navi extends HTMLElement {
       </div>
       <div class="pages">
         <a href="index.html" id="home" class="inactive"
-          ><img src="../resources/home_icon.svg" alt="Home Icon"> HOME</a
+          ><img src="../resources/navbar/home_icon.svg" alt="Home Icon"> HOME</a
         >
         <a href="upcoming.html" id="upcoming" class="inactive"
-          ><img src="../resources/upcoming_icon.svg" alt="Upcoming Icon"> UPCOMING</a
+          ><img src="../resources/navbar/upcoming_icon.svg" alt="Upcoming Icon"> UPCOMING</a
         >
         <a href="team.html" id="team" class="inactive"
-          ><img src="../resources/team_icon.svg" alt="Team Icon"> THE TEAM</a
+          ><img src="../resources/navbar/team_icon.svg" alt="Team Icon"> THE TEAM</a
         >
         <a href="about.html" id="archive" class="inactive"
-          ><img src="../resources/archive_icon.svg" alt="Archive Icon"> ARCHIVE</a
+          ><img src="../resources/navbar/archive_icon.svg" alt="Archive Icon"> ARCHIVE</a
         >
       </div>
       <div class="right">
         <a href="javascript:void(0)" onclick="toggle()" class="arrow"
-          ><img id="iconPic" src="../resources/Up.svg"
+          ><img id="iconPic" src="../resources/navbar/down.svg"
         /></a>
       </div>
     </div>
@@ -46,9 +46,9 @@ function toggle() {
     var x = document.getElementById("navbar");
     if (x.className === "topnav") {
       x.className += " open";
-      document.getElementById("iconPic").src = "../resources/Down.svg";
+      document.getElementById("iconPic").src = "../resources/navbar/up.svg";
     } else {
       x.className = "topnav";
-      document.getElementById("iconPic").src = "../resources/Up.svg";
+      document.getElementById("iconPic").src = "../resources/navbar/down.svg";
     }
   }
